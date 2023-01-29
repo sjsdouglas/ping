08634d0f607c802003d3db3916409923890ac87f
Douglas <104078248+sjsdouglas@users.noreply.github.com>
2023-01-29 13:22:14 -0300

    Update pyinstaller.py

--- ping via Console/pyinstaller.py
+++ ping via Console/pyinstaller.py
@@ -3,7 +3,8 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     r'mainC.py',
     r'-i=main.ico',
+    r'--version-file=versionfile.txt',
     '-F',
     '-c',
-    '-n P.I.N.G. C'
+    '-nPING C'
 ])
