df2fb5fb2d1223d8a26ecedd3add9027913e5002
Douglas <104078248+sjsdouglas@users.noreply.github.com>
2023-01-29 13:22:22 -0300

    Create pyinstaller_versionfile.py

---
+++ ping via Console/pyinstaller_versionfile.py
@@ -0,0 +1,12 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="versionfile.txt",
+    version="0.2.3",
+    company_name="github.com/sjsDouglas",
+    file_description="Shows ping via terminal with numbers and colors.",
+    internal_name="mainC",
+    legal_copyright="",
+    original_filename="PING C.exe",
+    product_name="PING Console"
+)
